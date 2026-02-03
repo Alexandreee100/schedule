4f6348a23537472fedc7c24dea7f3b2f234ae338
a.alekhin <alexander2709alexin@yandex.ru>
2026-02-04 01:55:55 +0300

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,31 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    printWidth: 120,
+    tabWidth: 4,
+    quoteProps: "consistent",
+    endOfLine: "lf",
+    overrides: [
+        {
+            files: "*.{js,jsx,ts,tsx}",
+            options: {
+                arrowParens: "always",
+                semi: true,
+                trailingComma: "es5",
+                singleQuote: false,
+                bracketSameLine: true,
+            },
+        },
+        {
+            files: ["*.json", "*.yml", "*.md"],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+    plugins: ["prettier-plugin-sh"],
+};
+
+export default config;
---
+++ vite.config.ts
@@ -0,0 +1,95 @@
+/// <reference types="vitest/config" />
+
+import importMetaEnv from "@import-meta-env/unplugin";
+import react from "@vitejs/plugin-react";
+import dns from "dns";
+import { defineConfig, loadEnv, type PluginOption } from "vite";
+import checker from "vite-plugin-checker";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+dns.setDefaultResultOrder("verbatim");
+
+const DEV_SERVER_PORT = 51268;
+
+export default defineConfig(({ mode }) => {
+    const envs = loadEnv(mode, process.cwd(), "");
+
+    const isDevMode = mode === "development";
+
+    const plugins: PluginOption[] = [
+        importMetaEnv.vite({ example: ".env.example", env: ".env" }),
+        react({
+            babel: {
+                assumptions: {
+                    setPublicClassFields: true,
+                },
+                presets: [
+                    /**
+                     * Парсим тайпскрипт через babel, чтобы иметь возможность использовать декораторы в абстрактных
+                     * классах https://github.com/babel/babel/pull/7850
+                     */
+                    [
+                        "@babel/preset-typescript",
+                        { isTSX: true, onlyRemoveTypeImports: true, optimizeConstEnums: true, allExtensions: true },
+                    ],
+                ],
+                plugins: [
+                    ["@babel/plugin-proposal-decorators", { version: "legacy" }],
+                    "@babel/plugin-transform-class-properties",
+                    "@babel/plugin-transform-class-static-block",
+                ],
+            },
+        }),
+        svgr({
+            svgrOptions: {
+                ref: true,
+            },
+        }),
+        tsconfigPaths(),
+    ];
+
+    if (isDevMode) {
+        plugins.push(
+            checker({
+                overlay: {
+                    initialIsOpen: "error",
+                },
+                typescript: true,
+                eslint: {
+                    lintCommand: "eslint",
+                    useFlatConfig: true,
+                },
+            })
+        );
+    }
+
+    return {
+        appType: "spa",
+        plugins,
+        base: envs.PUBLIC_URL,
+        build: {
+            sourcemap: "hidden",
+            target: ["es2020", "edge88", "firefox78", "chrome87", "safari14"],
+            outDir: "./build",
+            rollupOptions: {
+                output: {
+                    manualChunks(id) {
+                        if (id.includes("node_modules")) {
+                            return "vendor";
+                        }
+                    },
+                },
+            },
+        },
+        test: {
+            environment: "happy-dom",
+        },
+        server: {
+            port: DEV_SERVER_PORT,
+        },
+        preview: {
+            port: DEV_SERVER_PORT,
+        },
+    };
+});
