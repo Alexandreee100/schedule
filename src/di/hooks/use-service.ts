0301835a3abaa50b2b4377609fb98fcfe96f4145
a.alekhin <alexander2709alexin@yandex.ru>
2026-02-23 00:06:32 +0300

    Список учеников

--- src/di/hooks/use-service.ts
+++ src/di/hooks/use-service.ts
@@ -3,7 +3,7 @@ import { useRefFactory } from "src/shared/hooks/use-ref-factory";
 
 import { useContainer } from "@/di/hooks/use-container";
 
-export const useService = <T extends any>(type: ServiceIdentifier<T>): T => {
+export const useService = <T>(type: ServiceIdentifier<T>): T => {
     const container = useContainer();
     return useRefFactory(() => container.get(type));
 };
--- src/features/student-list/index.tsx
+++ src/features/student-list/index.tsx
@@ -1,1 +1,11 @@
+import { makeObservable } from "mobx";
+
+class StudentListViewModel {
+    constructor() {
+        makeObservable(this, {}, { autoBind: true });
+    }
+
+    public get columns() {}
+}
+
 const StudentList = () => {};
