14bd142921b5be5e28682c3ef844a973d5e2f50f
a.alekhin <alexander2709alexin@yandex.ru>
2026-05-18 01:02:05 +0300

    monorepo

--- apps/schedule/vite.config.ts
+++ apps/schedule/vite.config.ts
@@ -1,7 +1,6 @@
 import "vitest/config";
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv, type PluginOption } from "vite";
-import babel from "vite-plugin-babel";
 import checker from "vite-plugin-checker";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -12,36 +11,6 @@ export default defineConfig(({ mode }) => {
     const isDevMode = mode === "development";
 
     const plugins: PluginOption[] = [
-        babel({
-            babelConfig: {
-                assumptions: {
-                    setPublicClassFields: true,
-                },
-                presets: [
-                    /**
-                     * Парсим тайпскрипт через babel, чтобы иметь возможность использовать декораторы в абстрактных
-                     * классах https://github.com/babel/babel/pull/7850
-                     */
-                    [
-                        "@babel/preset-typescript",
-                        {
-                            isTSX: true,
-                            onlyRemoveTypeImports: true,
-                            optimizeConstEnums: true,
-                            allExtensions: true,
-                        },
-                    ],
-                ],
-                plugins: [
-                    [
-                        "@babel/plugin-proposal-decorators",
-                        { version: "legacy" },
-                    ],
-                    "@babel/plugin-transform-class-properties",
-                    "@babel/plugin-transform-class-static-block",
-                ],
-            },
-        }),
         react(),
         svgr({
             svgrOptions: {
